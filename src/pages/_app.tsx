d3046e792de544f6364d2ff08a7578bc9c640eeb
joe badawi <joebadawi9@gmail.com>
2025-09-05 14:50:49 +0300

    First gsap animations commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function App({ Component, pageProps }: AppProps) {
+  gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies
+  // register the effect with GSAP:
+
+  gsap.registerEffect({
+    name: "fade",
+    effect: (targets: any) => {
+      return gsap.fromTo(
+        targets,
+        { opacity: 0, y: "10px" },
+        { duration: 2, opacity: 1, y: "0px" }
+      );
+    },
+    defaults: { duration: 2 }, //defaults get applied to any "config" object passed to the effect
+    extendTimeline: true, //now you can call the effect directly on any GSAP timeline to have the result immediately inserted in the position you define (default is sequenced at the end)
+  });
+
+  // gsap.globalTimeline.timeScale(4); // accelerates ALL animations by x times
+
   return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,117 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
 
 export default function Home() {
+  gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies
+  const methodsTweenRef = useRef<gsap.core.Tween | null>(null);
+  const floatTweenRef = useRef<gsap.core.Tween | null>(null);
+
+  useGSAP(() => {
+    gsap.effects.fade(".box");
+    const tween = gsap.fromTo(
+      ".from-to",
+      { opacity: 0 },
+      { duration: 2, opacity: 1 }
+    );
+    tween.delay(1);
+    const methodsTween = gsap.fromTo(
+      ".tween-methods",
+      {
+        rotation: 0,
+        paused: true, // Start paused so we can control it with buttons
+      },
+      {
+        rotation: 360,
+        x: "400px",
+        duration: 5,
+        ease: "ease-in-out",
+        paused: true, // Start paused so we can control it with buttons
+      }
+    );
+    methodsTweenRef.current = methodsTween;
+
+    const floatTween = gsap.fromTo(
+      ".tween-methods",
+      {
+        y: 0,
+        paused: true, // Start paused so we can control it with buttons
+      },
+      {
+        y: "-20px",
+        ease: "ease-in-out",
+        paused: true, // Start paused so we can control it with buttons
+      }
+    );
+    floatTweenRef.current = floatTween;
+  });
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-[90%] mx-auto mt-10">
+      <div className="flex flex-col gap-10">
+        <div>
+          <p className="font-semibold">fade in effect using gsap effects</p>
+          <div className="box h-20 w-20 rounded-lg bg-red-500"></div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <p className="font-semibold">
+            fade in effect using gsap fromTo with a delay of 1 second
+          </p>
+          <div className="from-to h-20 w-20 rounded-lg bg-blue-500"></div>
+        </div>
+
+        <div>
+          <p className="font-semibold mb-3">using tween methods on target</p>
+          <div className="tween-methods h-20 w-20 rounded-lg bg-green-500"></div>
+
+          <div className="flex gap-3 items-center mt-4">
+            <div
+              onClick={() => methodsTweenRef.current?.play()}
+              className="cursor-pointer rounded-[30px] bg-green-400 px-5 py-1 flex items-center justify-center"
+            >
+              <p>Play</p>
+            </div>
+            <div
+              onClick={() => methodsTweenRef.current?.pause()}
+              className="cursor-pointer rounded-[30px] bg-red-400 px-5 py-1 flex items-center justify-center"
+            >
+              <p>Pause</p>
+            </div>
+            <div
+              onClick={() => methodsTweenRef.current?.reverse()}
+              className="cursor-pointer rounded-[30px] bg-gray-400 px-5 py-1 flex items-center justify-center"
+            >
+              <p>Reverse</p>
+            </div>
+            <div
+              onClick={() => methodsTweenRef.current?.restart()}
+              className="cursor-pointer rounded-[30px] bg-blue-400 px-5 py-1 flex items-center justify-center"
+            >
+              <p>Restart</p>
+            </div>
+            <div
+              onClick={() =>
+                methodsTweenRef.current?.yoyo(true).repeat(3).play()
+              }
+              className="cursor-pointer rounded-[30px] bg-green-400 px-5 py-1 flex items-center justify-center"
+            >
+              <p>Play Yoyo 3 times</p>
+            </div>
+            <div
+              onClick={() =>
+                methodsTweenRef.current
+                  ?.play()
+                  .then(() => floatTweenRef.current?.play())
+                  .then(() => floatTweenRef.current?.reverse())
+                  .then(() => methodsTweenRef.current?.reverse())
+              }
+              className="cursor-pointer rounded-[30px] bg-green-400 px-5 py-1 flex items-center justify-center"
+            >
+              <p>After initial animation do something else</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
